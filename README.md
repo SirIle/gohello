8d5cb28a7f22a16843a4cf7a24d4dc5475810c26
Ilkka Anttonen <ilkka.anttonen@accenture.com>
2016-04-04 22:57:01 +0300

    Tweaking to get demoable
    Changed the case of the JSON entities to lowercase. Added logging.
    Removed prettyprinting.

--- README.md
+++ README.md
@@ -4,34 +4,30 @@ This simple program is meant to be used to demonstrate scaling using Docker cont
 
 For building the container [golang-builder](https://github.com/CenturyLinkLabs/golang-builder) is used. The container can be built (assuming you have a working Docker environment) with:
 
-```
+```bash
 docker run --rm   -v "$(pwd):/src"   -v /var/run/docker.sock:/var/run/docker.sock   centurylink/golang-builder
 ```
 
 If you want to tag the image straight after the build the command is
 
-```
+```bash
 docker run --rm   -v "$(pwd):/src"   -v /var/run/docker.sock:/var/run/docker.sock   centurylink/golang-builder sirile/gohello
 ```
 
 After that running the container can be done with
 
-```
+```bash
 docker run --rm -p 80:80 gohello
 ```
 
 Testing the service can be done with
 
-```
+```bash
 curl http://<ip_of_docker_node>
 ```
 
 which should return something like
 
-```
-{
-  "Hostname": "76274eeb9b2d",
-  "Time": "2015-11-25T12:11:07.085817804Z",
-  "Language": "go"
- }
+```json
+{"hostname": "76274eeb9b2d","time": "2015-11-25T12:11:07.085817804Z","language": "go"}
 ```
--- hello.go
+++ hello.go
@@ -6,20 +6,28 @@ import (
 	"time"
 
 	"github.com/emicklei/go-restful"
+	"github.com/op/go-logging"
+)
+
+var log = logging.MustGetLogger("go-image-test")
+var format = logging.MustStringFormatter(
+	`%{color}%{time:15:04:05.000} %{shortfunc} ▶ %{level:.4s} %{id:03x}%{color:reset} %{message}`,
 )
 
 // Message defines the structure of the returned JSON
 type Message struct {
-	Hostname string
-	Time     time.Time
-	Language string
+	Hostname string    `json:"hostname"`
+	Time     time.Time `json:"time"`
+	Language string    `json:"language"`
 }
 
 func main() {
+	logging.SetBackend(logging.NewBackendFormatter(logging.NewLogBackend(os.Stderr, "", 0), format))
 	ws := new(restful.WebService)
 	ws.Route(ws.GET("/").To(hello))
 	ws.Route(ws.HEAD("/").To(header))
 	restful.Add(ws)
+	log.Debug("Starting the server and listening for requests")
 	http.ListenAndServe(":80", nil)
 }
 
@@ -27,10 +35,13 @@ func hello(req *restful.Request, resp *restful.Response) {
 	name, err := os.Hostname()
 	if err == nil {
 		msg := Message{name, time.Now(), "go"}
+		resp.PrettyPrint(false)
 		resp.WriteAsJson(msg)
 	}
+	log.Debug("JSON served")
 }
 
+// Implemented so that HAProxy heartbeat checks work properly
 func header(req *restful.Request, resp *restful.Response) {
 	resp.WriteHeader(200)
 }
